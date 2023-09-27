ef6fc3c61b063dcf88a8f56266d6223767df7061
sam <8333102+passaglia@users.noreply.github.com>
2023-09-27 16:51:56 +0900

    remove old files

--- jrank/jobs/get_supertorin_qa.sh
+++
@@ -1,4 +0,0 @@
-EVAL_DIR=/home/mdxuser/japanese-llm-ranking/jrank
-OUTPUT_DIR=$EVAL_DIR
-
-python $EVAL_DIR/get_super-torin_qa.py -q $EVAL_DIR/questions/rakuda_v1.jsonl -o $EVAL_DIR/answers/rakuda_v1/super-torin.jsonl > $OUTPUT_DIR/qa_torin.out 2> $OUTPUT_DIR/qa_torin.err
